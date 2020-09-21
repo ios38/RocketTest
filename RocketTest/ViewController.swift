b9d98f6418980bd1d1e384f8002a5169515e3def
Maksim Romanov <ios38ru@gmail.com>
2020-09-21 19:21:27 +0800

    Initial Commit

---
+++ RocketTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RocketTest
+//
+//  Created by Maksim Romanov on 21.09.2020.
+//  Copyright © 2020 Maksim Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
